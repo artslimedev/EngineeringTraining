99735d7a8ae5e944f6e496e01d6a0048ca5a7499
Paolo Joseph <Pjoseph2@totalwine.com>
2023-09-25 15:21:36 -0400

    TT-79

--- script.js
+++ script.js
@@ -6,12 +6,15 @@ console.log(`modalButton ${modalButton.outerHTML}`)
 
 modalButton.addEventListener('click', (e) => {
     console.log('clicked button!')
+    modalContainer.classList.toggle('hidden')
 })
-
-const modalContainer = document.getElementById('modalContainer')
-
-modalContainer.classList.toggle('hidden')
 
 const closeModalButton = document.getElementsByClassName('close-modal-button')
 
 console.log('closeModalButton', closeModalButton)
+
+closeModalButton[0].addEventListener('click', (e) => {
+    console.log('clicked close modal button')
+    const modalContainer = document.getElementById('modalContainer')
+    modalContainer.classList.toggle('hidden')
+})
